8cb487d0412ab511a806c8408db1831fdef3dd94
hnatali <hnatali123@rambler.ru>
2017-03-15 21:16:34 +0300

    Часть кода теста на создание контакта  (вход в систему) перенесен в метод инициализации фикстуры

--- adressbook-web-tests/src/test/java/ru/sqrt/pft/addressbook/ContactCreationTests.java
+++ adressbook-web-tests/src/test/java/ru/sqrt/pft/addressbook/ContactCreationTests.java
@@ -21,86 +21,40 @@ public class ContactCreationTests {
     public void setUp() throws Exception {
         wd = new FirefoxDriver();
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-    }
-    
-    @Test
-    public void ContactCreationTests() {
         wd.get("http://localhost/addressbook/index.php");
-        new Actions(wd).moveToElement(wd.findElement(By.id("LoginForm"))).build().perform();
-        new Actions(wd).moveToElement(wd.findElement(By.name("user"))).build().perform();
         wd.findElement(By.name("user")).click();
         wd.findElement(By.name("user")).clear();
         wd.findElement(By.name("user")).sendKeys("admin");
-        new Actions(wd).moveToElement(wd.findElement(By.id("LoginForm"))).build().perform();
-        new Actions(wd).moveToElement(wd.findElement(By.name("pass"))).build().perform();
         wd.findElement(By.name("user")).click();
         wd.findElement(By.name("pass")).click();
         wd.findElement(By.name("pass")).clear();
         wd.findElement(By.name("pass")).sendKeys("secret");
-        new Actions(wd).moveToElement(wd.findElement(By.id("LoginForm"))).build().perform();
-        new Actions(wd).moveToElement(wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]"))).build().perform();
         wd.findElement(By.name("pass")).click();
         wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
-        new Actions(wd).moveToElement(wd.findElement(By.id("content"))).build().perform();
-        new Actions(wd).moveToElement(wd.findElement(By.name("searchform"))).build().perform();
-        new Actions(wd).moveToElement(wd.findElement(By.id("search-az"))).build().perform();
-        new Actions(wd).moveToElement(wd.findElement(By.id("content"))).build().perform();
-        new Actions(wd).moveToElement(wd.findElement(By.id("nav"))).build().perform();
-        new Actions(wd).moveToElement(wd.findElement(By.linkText("add new"))).build().perform();
+    }
+    
+    @Test
+    public void testContactCreation() {
         wd.findElement(By.linkText("add new")).click();
-        new Actions(wd).moveToElement(wd.findElement(By.linkText("add new"))).build().perform();
-        new Actions(wd).moveToElement(wd.findElement(By.id("nav"))).build().perform();
-        new Actions(wd).moveToElement(wd.findElement(By.xpath("//div[@id='content']//h1[.='Edit / add address book entry']"))).build().perform();
-        new Actions(wd).moveToElement(wd.findElement(By.id("content"))).build().perform();
-        new Actions(wd).moveToElement(wd.findElement(By.name("theform"))).build().perform();
-        new Actions(wd).moveToElement(wd.findElement(By.name("firstname"))).build().perform();
         wd.findElement(By.name("firstname")).click();
         wd.findElement(By.name("firstname")).clear();
         wd.findElement(By.name("firstname")).sendKeys("name11");
-        new Actions(wd).moveToElement(wd.findElement(By.name("theform"))).build().perform();
-        new Actions(wd).moveToElement(wd.findElement(By.name("lastname"))).build().perform();
         wd.findElement(By.name("firstname")).click();
         wd.findElement(By.name("lastname")).click();
         wd.findElement(By.name("lastname")).clear();
         wd.findElement(By.name("lastname")).sendKeys("name12");
-        new Actions(wd).moveToElement(wd.findElement(By.name("theform"))).build().perform();
-        new Actions(wd).moveToElement(wd.findElement(By.name("address"))).build().perform();
         wd.findElement(By.name("lastname")).click();
         wd.findElement(By.name("address")).click();
         wd.findElement(By.name("address")).clear();
         wd.findElement(By.name("address")).sendKeys("address 11");
-        new Actions(wd).moveToElement(wd.findElement(By.name("theform"))).build().perform();
-        new Actions(wd).moveToElement(wd.findElement(By.name("homepage"))).build().perform();
-        new Actions(wd).moveToElement(wd.findElement(By.name("email3"))).build().perform();
-        new Actions(wd).moveToElement(wd.findElement(By.name("email2"))).build().perform();
-        new Actions(wd).moveToElement(wd.findElement(By.name("theform"))).build().perform();
-        new Actions(wd).moveToElement(wd.findElement(By.name("email"))).build().perform();
         wd.findElement(By.name("address")).click();
         wd.findElement(By.name("email")).click();
         wd.findElement(By.name("email")).clear();
         wd.findElement(By.name("email")).sendKeys("email11");
-        new Actions(wd).moveToElement(wd.findElement(By.name("theform"))).build().perform();
-        new Actions(wd).moveToElement(wd.findElement(By.name("work"))).build().perform();
-        new Actions(wd).moveToElement(wd.findElement(By.name("theform"))).build().perform();
-        new Actions(wd).moveToElement(wd.findElement(By.name("address2"))).build().perform();
-        new Actions(wd).moveToElement(wd.findElement(By.name("notes"))).build().perform();
-        new Actions(wd).moveToElement(wd.findElement(By.name("theform"))).build().perform();
-        new Actions(wd).moveToElement(wd.findElement(By.xpath("//div[@id='content']/form/input[21]"))).build().perform();
         wd.findElement(By.name("email")).click();
         wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
-        new Actions(wd).moveToElement(wd.findElement(By.cssSelector("html"))).build().perform();
-        new Actions(wd).moveToElement(wd.findElement(By.id("footer"))).build().perform();
-        new Actions(wd).moveToElement(wd.findElement(By.xpath("//div[@id='footer']//li[.='php-addressbook v8.2.5']"))).build().perform();
-        new Actions(wd).moveToElement(wd.findElement(By.id("content"))).build().perform();
-        new Actions(wd).moveToElement(wd.findElement(By.cssSelector("div.msgbox"))).build().perform();
-        new Actions(wd).moveToElement(wd.findElement(By.linkText("home page"))).build().perform();
         wd.findElement(By.linkText("home page")).click();
-        new Actions(wd).moveToElement(wd.findElement(By.id("content"))).build().perform();
-        new Actions(wd).moveToElement(wd.findElement(By.id("maintable"))).build().perform();
-        new Actions(wd).moveToElement(wd.findElement(By.cssSelector("body"))).build().perform();
-        new Actions(wd).moveToElement(wd.findElement(By.cssSelector("body"))).build().perform();
-        new Actions(wd).moveToElement(wd.findElement(By.cssSelector("html"))).build().perform();
-        new Actions(wd).moveToElement(wd.findElement(By.cssSelector("html"))).build().perform();
+
     }
     
     @AfterMethod
