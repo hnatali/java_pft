3cd58c8546d68227773fc0b93aa7fa3f839e2420
hnatali <hnatali123@rambler.ru>
2017-03-29 12:16:28 +0300

    Сравнение списков в тестах на удаление контактов и групп

--- adressbook-web-tests/src/test/java/ru/sqrt/pft/addressbook/tests/ContactDeletionTests.java
+++ adressbook-web-tests/src/test/java/ru/sqrt/pft/addressbook/tests/ContactDeletionTests.java
@@ -26,5 +26,7 @@ public class ContactDeletionTests extends TestBase
     app.getNavigationHelper().gotoHomePage();
     List<ContactData>  after = app.getContactHelper().getContactList();
     Assert.assertEquals(after.size(), before.size() - 1);
+    before.remove(before.size() - 1);
+    Assert.assertEquals (before, after);
   }
 }
