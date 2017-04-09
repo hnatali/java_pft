a817b0cdcce3c9555036fe774c269a24e9b68462
hnatali <hnatali123@rambler.ru>
2017-04-09 12:38:05 +0300

    Тесты для группы - кэширование результатов длительных операций

--- adressbook-web-tests/src/test/java/ru/sqrt/pft/addressbook/appmeneger/GroupHelper.java
+++ adressbook-web-tests/src/test/java/ru/sqrt/pft/addressbook/appmeneger/GroupHelper.java
@@ -68,6 +68,7 @@ public class GroupHelper extends HelperBase
     initGroupCreation();
     fillGroupForm(group);
     submitGroupCreation();
+    groupCache = null;
     returnToGroupPage();
   }
 
@@ -76,6 +77,7 @@ public class GroupHelper extends HelperBase
     initGroupModification ();
     fillGroupForm(group);
     sabmitGroupModification ();
+    groupCache = null;
     returnToGroupPage();
   }
 
@@ -83,6 +85,7 @@ public class GroupHelper extends HelperBase
   public void delete(GroupData group) {
     selectGroupById(group.getId());
     deleteSelectedGroups();
+    groupCache = null;
     returnToGroupPage();
   }
 
@@ -98,17 +101,22 @@ public class GroupHelper extends HelperBase
   }
 
 
+  private Groups groupCache = null;
+
   public Groups all()
   {
-    Groups groups = new Groups();
+    if (groupCache != null) {
+      return  new Groups(groupCache);
+    }
+    groupCache = new Groups();
     List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
     for (WebElement element : elements)
     {
       String name = element.getText();
       int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("value"));
-      groups.add(new GroupData().withId(id).withIName(name));
+      groupCache.add(new GroupData().withId(id).withIName(name));
     }
-    return groups;
+    return new Groups(groupCache);
   }
 
 
