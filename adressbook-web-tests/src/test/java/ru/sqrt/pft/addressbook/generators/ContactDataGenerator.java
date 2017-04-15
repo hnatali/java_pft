7071392fd160fabc9d13836a53a82cbcd3b62b68
hnatali <hnatali123@rambler.ru>
2017-04-16 00:20:50 +0300

    Контакт - реализован генератор тестовых данных, позволяющий  сохранять данные в  формате JSON

--- adressbook-web-tests/src/test/java/ru/sqrt/pft/addressbook/generators/ContactDataGenerator.java
+++ adressbook-web-tests/src/test/java/ru/sqrt/pft/addressbook/generators/ContactDataGenerator.java
@@ -4,8 +4,9 @@ package ru.sqrt.pft.addressbook.generators;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import ru.sqrt.pft.addressbook.model.ContactData;
-import ru.sqrt.pft.addressbook.model.GroupData;
 
 
 import java.io.File;
@@ -23,6 +24,8 @@ public class ContactDataGenerator
   @Parameter (names = "-f", description = "Target file")
   public String file;
 
+  @Parameter (names = "-d", description = "Data format")
+  public String format;
 
   public  static  void  main(String[] arqs) throws IOException
   {
@@ -41,10 +44,28 @@ public class ContactDataGenerator
 
   private void run() throws IOException {
     List<ContactData> contacts = genereateContacts(count);
-    save(contacts, new File(file));
+    if (format.equals("csv"))
+    {
+      saveAsCsv(contacts, new File(file));
+    } else if (format.equals("json")) {
+      saveAsJson(contacts, new File(file));
+    } else {
+      System.out.println("Неизвестный формат" + format);
+    }
+
+    }
+
+  private void saveAsJson(List<ContactData> contacts, File file) throws IOException {
+    Gson gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
+    String json = gson.toJson(contacts);
+    Writer writer = new FileWriter(file);
+    writer.write(json);
+    writer.close();
+
   }
 
-  private static void save(List<ContactData> contacts, File file) throws IOException {
+
+  private static void saveAsCsv(List<ContactData> contacts, File file) throws IOException {
     Writer writer = new FileWriter(file);
     for (ContactData contact : contacts)
     {
--- adressbook-web-tests/src/test/java/ru/sqrt/pft/addressbook/model/ContactData.java
+++ adressbook-web-tests/src/test/java/ru/sqrt/pft/addressbook/model/ContactData.java
@@ -1,20 +1,34 @@
 package ru.sqrt.pft.addressbook.model;
 
+import com.google.gson.annotations.Expose;
+
 import java.io.File;
 
 public class ContactData {
   private int id = Integer.MAX_VALUE;
+  @Expose
   private  String firstname;
+  @Expose
   private  String lastname;
+  @Expose
   private  String address;
+  @Expose
   private  String email;
+  @Expose
   private  String email2;
+  @Expose
   private  String email3;
+  @Expose
   private String group;
+  @Expose
   private String homePhone;
+  @Expose
   private String mobilePhone;
+  @Expose
   private String workPhone;
+  @Expose
   private String allPhones;
+  @Expose
   private String allEmails;
   private File photo;
 
